d8c0fe1f29f42fc909cb93afc7282e0fd1025551
munoadawafat <tmunodawafa84@gmail.com>
2026-09-05 23:57:02 +0200

    while loop

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is your magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        Random RandomGenerator = new();
+        int magicNumber = RandomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int guessCount = 0;
+        
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! in {guessCount} guesses");
+            }
+        }    
     }
 }
