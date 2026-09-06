1bdb3b07a337a73a0cc5c74d69e3f73ca1f7c32e
munoadawafat <tmunodawafa84@gmail.com>
2026-09-06 15:12:53 +0200

    list and generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;        
+
+        Console.WriteLine("Enter 0 to quit.");
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+        
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } 
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"Sum: {sum}");
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"Average: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"Maximum: {max}");
     }
 }
